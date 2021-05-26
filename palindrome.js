300412b063612976f0eee43553e622af84487d5b
Conor <c17730535@mydit.ie>
2021-05-27 00:18:14 +0100

    pushing to github

--- palindrome.js
+++ palindrome.js
@@ -3,13 +3,13 @@ function palindrome(str) {
   // remove all non-aplahnumeric chracter
   // convert to all lowercase
   str = str.toLowerCase();
-  str = str.replace(/_/g,"");
+  str = str.replace(/_/g, "");
   str = str.match(/\w/g);
 
   // floor the divison
-  // if even number of letters 2nd half = first half reversed
-  // else odd number of letters a a b a a
-  // str.length/2 - 1 = str.lenght / 2 -> str.length
+  // if even number of letters: 2nd half = first half reversed
+  // else odd number of letters: a a b a a
+  // first half - index of middle letter = second half + index of middle letter
   if (str.length % 2 == 0) {
     let firstHalf = str.splice(0, str.length / 2);
     if (firstHalf.join("") == str.reverse().join("")) return true;
