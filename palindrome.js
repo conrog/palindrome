992a48f4a482aa2d8d83acd70712f92ea86f7cfe
Conor <c17730535@mydit.ie>
2021-05-27 01:05:02 +0100

    refactored code and removed comments

--- palindrome.js
+++ palindrome.js
@@ -1,22 +1,15 @@
 function palindrome(str) {
-  // tidy up the string:
-  // remove all non-aplahnumeric chracter
-  // convert to all lowercase
-  str = str.toLowerCase();
-  str = str.replace(/_/g, "");
-  str = str.match(/\w/g);
+  str = str.toLowerCase().replace(/_/g, "").match(/\w/g);
 
-  // floor the divison
-  // if even number of letters: 2nd half = first half reversed
-  // else odd number of letters: a a b a a
-  // first half - index of middle letter = second half + index of middle letter
   if (str.length % 2 == 0) {
-    let firstHalf = str.splice(0, str.length / 2);
-    if (firstHalf.join("") == str.reverse().join("")) return true;
+    if (str.splice(0, str.length / 2).join("") == str.reverse().join(""))
+      return true;
   } else {
-    let firstHalf = str.splice(0, Math.floor(str.length / 2));
-    let secondHalf = str.slice(1);
-    if (firstHalf.join("") == secondHalf.reverse().join("")) return true;
+    if (
+      str.splice(0, Math.floor(str.length / 2)).join("") ==
+      str.slice(1).reverse().join("")
+    )
+      return true;
   }
 
   return false;
